62cdaf716edc911515cdf630f7b496c5e644c51f
studiorabota <git@studiorabota.com>
2025-01-26 13:44:55 +0100

    Temporary turn off soil moisture sensor

--- src/wiggle_sensors/hourly.py
+++ src/wiggle_sensors/hourly.py
@@ -1,11 +1,11 @@
 from wiggle_sensors.environment import write_bme680
 from wiggle_sensors.soil_temperature import write_DS18B20
-from wiggle_sensors.soil_moisture import write_soil_moisture
+# from wiggle_sensors.soil_moisture import write_soil_moisture
 
 def main():
     write_bme680()
     write_DS18B20()
-    write_soil_moisture()
+    # write_soil_moisture()
 
 if __name__ == "__main__":
     main()
